28c217a69650f91a0da100296459a5ddd95cc239
DaviMarqs <davi.amqs@hotmail.com>
2022-07-14 11:22:10 -0300

    fix sql bug

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import { adminJs, adminJsRouter } from "./adminjs";
 import { sequelize } from "./database";
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.use(adminJs.options.rootPath, adminJsRouter);
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,4 +15,7 @@ app.listen(PORT, () => {
     console.log("DB connection successfull");
   });
   console.log(`Server started successfuly at port ${PORT}`);
+  sequelize.models.categories.findAll().then((result) => {
+    console.log(result);
+  });
 });
